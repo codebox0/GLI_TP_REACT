3b8919daa71b4ae35b4999d57cb8bbb0d6225ae8
Erwan IQUEL <erwan.iquel@gmail.com>
2018-10-16 23:10:18 +0200

    Move components files to components folder

--- erwan-iquel-ila/src/index.js
+++ erwan-iquel-ila/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import { Tricount } from './Tricount';
+import { Tricount } from './components/Tricount';
+import configureStore from './configureStore';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<Tricount />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Tricount />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
